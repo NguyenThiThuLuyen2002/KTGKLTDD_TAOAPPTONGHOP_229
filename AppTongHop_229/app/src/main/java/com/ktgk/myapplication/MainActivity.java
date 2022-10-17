a7d87119ceae7ebb881c97fc352468fef38acbd5
tluyen2602@gmail.com <tluyen2602@gmail.com>
2022-10-18 02:14:12 +0700

    second cm add Fragment to Main

--- AppTongHop_229/app/src/main/java/com/ktgk/myapplication/MainActivity.java
+++ AppTongHop_229/app/src/main/java/com/ktgk/myapplication/MainActivity.java
@@ -1,14 +1,45 @@
 package com.ktgk.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.ktgk.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigatioinView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case  R.id.setting:
+                    replaceFragment(new SettingFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
