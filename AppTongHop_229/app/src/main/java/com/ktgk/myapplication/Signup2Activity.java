ac77dd9af742ef90bf0abd83f67bdd8439657a3b
tluyen2602@gmail.com <tluyen2602@gmail.com>
2022-10-21 22:40:32 +0700

    sua fragment profile

--- AppTongHop_229/app/src/main/java/com/ktgk/myapplication/Signup2Activity.java
+++ AppTongHop_229/app/src/main/java/com/ktgk/myapplication/Signup2Activity.java
@@ -3,7 +3,9 @@ package com.ktgk.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signup2Activity extends AppCompatActivity {
     private AppCompatButton btnsignup;
@@ -11,5 +13,14 @@ public class Signup2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup2);
+
+        btnsignup= (AppCompatButton) findViewById(R.id.btn_signup);
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Signup2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
